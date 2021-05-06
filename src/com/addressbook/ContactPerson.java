63bed857c25ecf0ec50df0160a28b0faf2da5b92
Pratap Pawar <pratappawar1997@gmail.com>
2021-05-06 08:31:48 +0530

    Added:  Address Book created

---
+++ src/com/addressbook/ContactPerson.java
@@ -0,0 +1,28 @@
+package com.addressbook;
+
+public class ContactPerson {
+    private String first_name;
+    private String last_name;
+    private String address;
+    private String city;
+    private String state;
+    private int zip;
+    private long phone_number;
+    private String email;
+
+    public ContactPerson() {
+
+    }
+
+    public ContactPerson(String first_name, String last_name, String address, String city, String state, int zip, long phone_number, String email) {
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phone_number = phone_number;
+        this.email = email;
+    }
+}
+
